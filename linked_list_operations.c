c67ecc7421ac68ce8385d5b52e0313ba946c2529
Asᴍᴀ P A <120763637+asmapa@users.noreply.github.com>
2024-02-08 19:36:25 +0530

    Update linked_list_operations.c

--- linked_list_operations.c
+++ linked_list_operations.c
@@ -1,179 +1,223 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
-struct node {
+struct node{
     int data;
-    struct node *link;
+    struct node *next;
 };
 
-void insert_at_front(struct node *header, int x) {
-    struct node *new = (struct node*) malloc(sizeof(struct node));
-    new->data = x;
-    new->link = header->link;
-    header->link = new;
+struct node *createNode(int key)
+{
+    struct node *new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->data=key;
+    new_node->next=NULL;
 }
 
-void display(struct node *header) {
-    struct node *ptr = header->link;
+struct node *IF(struct node *head,int data)
+{
+    struct node *no=createNode(data);
 
-    printf("THE ELEMENTS ARE:\n");
-
-    while (ptr != NULL) {
-        printf("%d\t", ptr->data);
-        ptr = ptr->link;
+    if(head == NULL)
+    {
+        head=no;
     }
+    else
+    {
+        no->next=head;
+        head=no;
+
+    }
+    return head;
 }
 
-void insert_at_end(struct node *header, int data) {
-    struct node *new_node = (struct node*) malloc(sizeof(struct node));
-    new_node->data = data;
-    new_node->link = NULL;
-
-    struct node *ptr = header;
-    while (ptr->link != NULL) {
-        ptr = ptr->link;
+struct node *IB(struct node *head,int data)
+{
+    struct node *no=createNode(data);
+    if(head==NULL)
+    {
+        head=no;
     }
-
-    ptr->link = new_node;
-}
-
-void insert_in_between(struct node *header, int data, int position) {
-    struct node *new_node = (struct node*) malloc(sizeof(struct node));
-    new_node->data = data;
-
-    struct node *ptr = header;
-    int count = 0;
-
-    while (ptr != NULL && count < position - 1) {
-        ptr = ptr->link;
-        count++;
-    }
-
-    new_node->link = ptr->link;
-    ptr->link = new_node;
-}
-
-void delete_at_front(struct node *header) {
-    if (header->link != NULL) {
-        struct node *temp = header->link;
-        header->link = temp->link;
-        free(temp);
-    } else {
-        printf("List is empty\n");
-    }
-}
-
-void delete_at_end(struct node *header) {
-    struct node *ptr = header;
-    struct node *prev = NULL;
-
-    if (header->link == NULL) {
-        printf("List is empty\n");
-        return;
-    }
-
-    while (ptr->link != NULL) {
-        prev = ptr;
-        ptr = ptr->link;
-    }
-
-    prev->link = NULL;
-    free(ptr);
-}
-
-void delete_any(struct node *header, int position) {
-    if (position < 1) {
-        printf("Invalid position. Position must be 1 or greater.\n");
-        return;
-    }
-
-    struct node *ptr = header->link;  // Start from the first node, not the header
-    struct node *temp = header;
-    int count = 1;  // Start from position 1
-
-    while (ptr != NULL && count < position) {
-        temp = ptr;
-        ptr = ptr->link;
-        count++;
-    }
-
-    if (ptr == NULL) {
-        printf("Position %d is out of bounds. No node to delete.\n", position);
-        return;
-    }
-
-    temp->link = ptr->link;
-    free(ptr);
-}
-
-int main() {
-    int n, num, x, position;
-    struct node *header = (struct node*) malloc(sizeof(struct node));
-    header->link = NULL;
-    header->data = 0;
-    struct node *ptr = header;
-
-    printf("Enter the number of elements you need to add into the linked list:\n");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        struct node *temp = (struct node*) malloc(sizeof(struct node));
-        printf("Enter a number to insert: ");
-        scanf("%d", &num);
-        temp->data = num;
-        temp->link = NULL;
-        ptr->link = temp;
-        ptr = ptr->link;
-    }
-
-    ptr = header->link;
-
-    printf("THE ELEMENTS ARE:\n");
-
-    while (ptr != NULL) {
-        printf("%d\t", ptr->data);
-        ptr = ptr->link;
-    }
-
-    printf("\nEnter choice to perform other operations:\n");
-    printf("1. Insert at front\n");
-    printf("2. Insert at end\n");
-    printf("3. Insert in between\n");
-    printf("4. Delete at front\n");
-    printf("5. Delete at end\n");
-    printf("6. Delete from between\n");
-
-    int choice;
-    while (1) {
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                printf("Enter an element to insert at the front: ");
-                scanf("%d", &x);
-                insert_at_front(header, x);
-                break;
-            case 2:
-                display(header);
-                break;
-            case 3:
-                printf("Enter the element to insert at the end: ");
-                scanf("%d", &x);
-                insert_at_end(header, x);
-                break;
-            case 4:
-                delete_at_front(header);
-                break;
-            case 5:
-                delete_at_end(header);
-                break;
-            case 6:
-                printf("Enter the position to delete (1-based): ");
-                scanf("%d", &position);
-                delete_any(header, position);
-                break;
+    else
+    {
+        struct node *current=head;
+        while(current->next !=NULL)
+        {
+            current=current->next;
         }
+        current->next=no;
+    }
+    return head;
+}
+
+struct node *IM(struct node *head,int key,int data)
+{
+    struct node *no=createNode(data);
+    if(head==NULL)
+    {
+        head=no;
+    }
+    else
+    {
+        struct node *current=head;
+        while(current->next != NULL && current->data !=key)
+        {
+            current=current->next;
+        }
+        no->next=current->next;
+        current->next=no;
+    }
+    return head;
+}
+
+struct node *DF(struct node *head)
+{
+    if(head==NULL)
+    {
+        printf("it is empty");
+    }
+    struct node *current=head;
+    head=current->next;
+    free(current);
+    return head;
+
+    
+}
+
+struct node *DE(struct node *head)
+{
+    struct node *current=head;
+    struct node *prev=NULL;
+    while(current->next !=NULL)
+    {
+        prev=current;
+        current=current->next;
+    }
+    prev->next=NULL;
+    free(current);
+
+    return head;
+}
+
+struct node *DM(struct node *head,int key)
+{
+    struct node *current=head;
+    struct node *prev=NULL;
+    while(current !=NULL && current->data !=key)
+    {
+        prev=current;
+        current=current->next;
+    }
+    prev->next=current->next;
+    free(current);
+
+    return head;
+}
+
+struct node *reverse(struct node *head)
+{
+    struct node *next=NULL;
+    struct node *prev=NULL;
+    struct node *current=head;
+
+    while(current !=NULL)
+    {
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    return prev;
+}
+
+struct node *sort(struct node *head)
+{
+    struct node *current=head;
+    while(current !=NULL)
+    {
+        struct node *min=current;
+        struct node *temp=current->next;
+        while(temp != NULL)
+        {
+            if(temp->data < min->data)
+            {
+                min=temp;
+            }
+            temp=temp->next;
+        }
+
+        int t=current->data;
+        current->data=min->data;
+        min->data=t;
+
+        current=current->next;
+    }
+
+      return head;  
+}
+
+void display(struct node *head)
+{
+    struct node *current=head;
+    while(current !=NULL)
+    {
+        printf("%d",current->data);
+        printf("\t");
+        current=current->next;
     }
 }
 
-   
+int main()
+{
+    struct node *head;
+ 
+        
+    while(1){
+     printf("enter a choice:");
+    int choice;
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:printf("enter data:");
+        int data;
+        scanf("%d",&data);
+        head=IF(head,data);
+        break;
+
+        case 2:printf("enter:");
+        int da;
+        scanf("%d",&da);
+        head=IB(head,da);
+        break;
+
+        case 3:printf("Enter a number after that you can insert new node:");
+        int dat;
+        scanf("%d",&dat);
+        printf("enter data for insert:");
+        int d;
+        scanf("%d",&d);
+        head=IM(head,dat,d);
+        break;
+
+        case 4:head=DF(head);
+        break;
+
+        case 5:head=DE(head);
+        break;
+
+        case 6:printf("enter the data you want to delete:");
+        int g;
+        scanf("%d",&g);
+        head=DM(head,g);
+        break;
+
+        case 7:head=reverse(head);
+        break;
+
+        case 8:head=sort(head);
+        break;
+
+        case 9:display(head);
+        break;
+    }
+    }
+}
